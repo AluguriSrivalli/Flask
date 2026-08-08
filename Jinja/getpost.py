4873981a93c12c4a08fb2ec4ee972c5ae4cb3136
AluguriSrivalli <78839083+AluguriSrivalli@users.noreply.github.com>
2026-08-08 15:06:41 +0530

    Initialize Flask app with multiple routes
    Create a Flask application with routes for welcome, index, form, and about pages.

---
+++ Jinja/getpost.py
@@ -0,0 +1,30 @@
+from flask import Flask,render_template,request
+''' 
+    It create an instance of the flask class,
+    which will be your WSGI(Web Server Gateway Interface) application
+'''
+##WSGI Application
+app=Flask(__name__)     #entry point
+
+@app.route("/")
+def welcome():
+    return "<html><H1>Let us focus on intergating HTML tags</H1></html>"
+
+@app.route("/index",methods=['GET'])
+def index():
+    return render_template('index.html')  #It looks for index.html under templates folder
+    ''' If index.html is not found it will through TemplateNotFound error in webpage'''
+
+@app.route("/form",methods=['GET','POST'])
+def form():
+    if request.method=='POST':
+        name=request.form['name']
+        return f'Hello {name}'
+    return render_template('form.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+if __name__=="__main__":
+    app.run(debug=True)           #runs the entire app
